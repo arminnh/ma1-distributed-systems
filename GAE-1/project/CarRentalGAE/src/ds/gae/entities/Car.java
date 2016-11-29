c6c86d0423789b25d1534a27480418351684286a
jagog <jago.gyselinck@gmail.com>
2016-11-29 12:35:17 +0100

    illuminati

---
+++ GAE-1/project/CarRentalGAE/src/ds/gae/entities/Car.java
@@ -0,0 +1,80 @@
+package ds.gae.entities;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.jdo.annotations.Persistent;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+public class Car {
+	
+	
+    private int id;
+	
+    
+	private CarType type;
+	
+	
+    private Set<Reservation> reservations;
+
+    /***************
+     * CONSTRUCTOR *
+     ***************/
+    
+    public Car(int uid, CarType type) {
+    	this.id = uid;
+        this.type = type;
+        this.reservations = new HashSet<Reservation>();
+    }
+    
+    public Car(){}
+
+    /******
+     * ID *
+     ******/
+    
+    public int getId() {
+    	return id;
+    }
+    
+    /************
+     * CAR TYPE *
+     ************/
+    
+    public CarType getType() {
+        return type;
+    }
+
+    /****************
+     * RESERVATIONS *
+     ****************/
+    
+    public Set<Reservation> getReservations() {
+    	return reservations;
+    }
+
+    public boolean isAvailable(Date start, Date end) {
+        if(!start.before(end))
+            throw new IllegalArgumentException("Illegal given period");
+
+        for(Reservation reservation : reservations) {
+            if(reservation.getEndDate().before(start) || reservation.getStartDate().after(end))
+                continue;
+            return false;
+        }
+        return true;
+    }
+    
+    public void addReservation(Reservation res) {
+        reservations.add(res);
+    }
+    
+    public void removeReservation(Reservation reservation) {
+        // equals-method for Reservation is required!
+        reservations.remove(reservation);
+    }
+}
